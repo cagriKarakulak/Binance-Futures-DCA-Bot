a74482cf59f6d16ee0bc763014e17968038d2812
Çağrı Karakulak <33554156+cagriKarakulak@users.noreply.github.com>
2022-02-27 20:18:50 +0300

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -3,9 +3,7 @@
 import config
 import pandas as pd
 import ccxt
-import winsound
-duration = 1000  # milliseconds
-freq = 440  # Hz
+from smtplib import SMTP
 
 # SETTİNGS
 symbolName = input("Symbol (BTC, ETH, LTC...): ").upper()
@@ -16,16 +14,36 @@ maxSafetyTradesCount = float(input("Max Safety Trades Count: "))
 priceDeviation = float(input("Price Deviation %: "))
 safetyOrderStepScale = float(input("Safety Order Step Scale: "))
 safetyOrderVolumeScale = float(input("Safety Order Volume Scale: "))
-takeProfit = float(input("Take Profit %: "))
-stopLoss = float(input("Stop Loss %: "))
+takeProfitType = float(input("Take Profit Type (Classic TP(1) - Trailing TP(2)): "))
+if takeProfitType == 1:
+    takeProfit = float(input("Take Profit %: "))
+if takeProfitType == 2:
+    takeProfitTrigger = float(input("Trailing Take Profit Trigger %: "))
+    takeProfitTrailing = float(input("Trailing Take Profit %: "))
+SLselection = float(input("Do you want stop loss? = YES(1) - NO(2): "))
+if SLselection == 1:
+    stopLossType = float(input("Stop Loss Type = Classic SL(1) - Trailing SL(2): "))
+    if stopLossType == 1:
+        stopLoss = float(input("Stop Loss %: "))
+    if stopLossType == 2:
+        stopLossTrailing = float(input("Trailing Stop Loss %: "))
 positionSide = float(input("Position Side = Only Long(1) - Only Short(2) - Long and Short(3): "))
+profitAmount = float(input("Profit Amount: "))
+mail = float(input("Bot Send e-mail? (YES(1) - NO(2)): "))
+
 
 #ATTRIBUTES
 first = True
+longTPtrigger = False
+shortTPtrigger = False
+lastOrderPrice = 0
 tradeCount = 0
 symbol = symbolName+"/USDT"
 mainSafetyOrderSize = safetyOrderSize
 mainPriceDeviation = priceDeviation
+takeProfitCount = 0
+highestPrice = 0
+lowestPrice = 0
 
 # API CONNECT
 exchange = ccxt.binance({
@@ -70,36 +88,42 @@ while True:
         bars = exchange.fetch_ohlcv(symbol, timeframe="1m", since = None, limit = 1)
         df = pd.DataFrame(bars, columns=["timestamp", "open", "high", "low", "close", "volume"])
         
+        # Get current price
+        currentPrice = float(df["close"][len(df.index) - 1])
+
         # Starting price
         if first:
             firstPrice = float(df["close"][len(df.index) - 1])
+            highestPrice = currentPrice
+            lowestPrice = currentPrice
             first = False
+            
 
-        currentPrice = float(df["close"][len(df.index) - 1])
         
         # LONG ENTER
         def longEnter(alinacak_miktar):
             order = exchange.create_market_buy_order(symbol, alinacak_miktar)
-            winsound.Beep(freq, duration)
+
             
         # LONG EXIT
         def longExit():
             order = exchange.create_market_sell_order(symbol, float(position_info["positionAmt"][len(position_info.index) - 1]), {"reduceOnly": True})
-            winsound.Beep(freq, duration)
+
 
         # SHORT ENTER
         def shortEnter(alincak_miktar):
             order = exchange.create_market_sell_order(symbol, alincak_miktar)
-            winsound.Beep(freq, duration)
+
             
         # SHORT EXIT
         def shortExit():
             order = exchange.create_market_buy_order(symbol, (float(position_info["positionAmt"][len(position_info.index) - 1]) * -1), {"reduceOnly": True})
-            winsound.Beep(freq, duration)
+
 
         if inPosition == False:
             priceDeviation = mainPriceDeviation
             safetyOrderSize = mainSafetyOrderSize
+
         
         # LONG ENTER
         if firstPrice - (firstPrice/100) * priceDeviation >= currentPrice and shortPosition == False and maxSafetyTradesCount>tradeCount and float(free_balance["USDT"]) >= baseOrderSize and (positionSide == 1 or positionSide == 3):
@@ -114,6 +138,17 @@ while True:
             print("LONG ENTER")
             first = True
             tradeCount = tradeCount + 1
+            if tradeCount >= maxSafetyTradesCount:
+                lastOrderPrice = currentPrice
+            if mail == 1:
+                baslik = symbol
+                message = "LONG ENTER\n" + "TOTAL MONEY: " + str(balance['total']["USDT"])
+                content = f"Subject: {baslik}\n\n{message}"
+                mail = SMTP("smtp.gmail.com", 587)
+                mail.ehlo()
+                mail.starttls()
+                mail.login(config.mailAddress, config.password)
+                mail.sendmail(config.mailAddress, config.mailAddress, content.encode("utf-8"))
         
         # SHORT ENTER
         if ((firstPrice / 100) * priceDeviation) + firstPrice <= currentPrice and longPosition == False and maxSafetyTradesCount>tradeCount and float(free_balance["USDT"]) >= baseOrderSize and (positionSide == 2 or positionSide == 3): 
@@ -128,46 +163,220 @@ while True:
             print("SHORT ENTER")
             first = True
             tradeCount = tradeCount + 1
+            if tradeCount >= maxSafetyTradesCount:
+                lastOrderPrice = currentPrice
+            if mail == 1:
+                baslik = symbol
+                message = "SHORT ENTER\n" + "TOTAL USDT: " + str(balance['total']["USDT"])
+                content = f"Subject: {baslik}\n\n{message}"
+                mail = SMTP("smtp.gmail.com", 587)
+                mail.ehlo()
+                mail.starttls()
+                mail.login(config.mailAddress, config.password)
+                mail.sendmail(config.mailAddress, config.mailAddress, content.encode("utf-8"))
             
             
-        # LONG TAKE PROFIT
-        if longPosition and ((float(position_info["entryPrice"][len(position_info.index) - 1])/100)*takeProfit)+float(position_info["entryPrice"][len(position_info.index) - 1]) < currentPrice and (positionSide == 1 or positionSide == 3):
+        # LONG TAKE PROFIT - CLASSIC
+        if takeProfitType == 1 and longPosition and ((float(position_info["entryPrice"][len(position_info.index) - 1])/100)*takeProfit)+float(position_info["entryPrice"][len(position_info.index) - 1]) <= currentPrice and (positionSide == 1 or positionSide == 3):
             print("TAKE PROFIT")
             longExit()
             first = True
             tradeCount = 0
-            
-        # SHORT TAKE PROFIT
-        if shortPosition and float(position_info["entryPrice"][len(position_info.index) - 1]) - (float(position_info["entryPrice"][len(position_info.index) - 1])/100) * takeProfit >= currentPrice and (positionSide == 2 or positionSide == 3):
+            if mail == 1:
+                baslik = symbol
+                message = "LONG TAKE PROFIT\n" + "TOTAL USDT: " + str(balance['total']["USDT"])
+                content = f"Subject: {baslik}\n\n{message}"
+                mail = SMTP("smtp.gmail.com", 587)
+                mail.ehlo()
+                mail.starttls()
+                mail.login(config.mailAddress, config.password)
+                mail.sendmail(config.mailAddress, config.mailAddress, content.encode("utf-8"))
+            takeProfitCount = takeProfitCount+ 1
+
+        # get highest price
+        if currentPrice > highestPrice:
+            highestPrice = currentPrice
+        else: highestPrice = highestPrice
+
+        # LONG TAKE PROFIT - TRAILING TRIGGER
+        if takeProfitType == 2 and longPosition and ((float(position_info["entryPrice"][len(position_info.index) - 1])/100)*takeProfitTrigger)+float(position_info["entryPrice"][len(position_info.index) - 1]) <= currentPrice:
+            longTPtrigger = True
+
+        # LONG TAKE PROFIT - TRAILING
+        if takeProfitType == 2 and longTPtrigger and longPosition and highestPrice - (highestPrice/100) * takeProfitTrailing >= currentPrice  and (positionSide == 1 or positionSide == 3):
+            print("TRAILING TAKE PROFIT")
+            longExit()
+            longTPtrigger = False
+            first = True
+            tradeCount = 0
+            if mail == 1:
+                baslik = symbol
+                message = "LONG TRAILING TAKE PROFIT\n" + "TOTAL USDT: " + str(balance['total']["USDT"])
+                content = f"Subject: {baslik}\n\n{message}"
+                mail = SMTP("smtp.gmail.com", 587)
+                mail.ehlo()
+                mail.starttls()
+                mail.login(config.mailAddress, config.password)
+                mail.sendmail(config.mailAddress, config.mailAddress, content.encode("utf-8"))
+            takeProfitCount = takeProfitCount+ 1
+
+
+        # SHORT TAKE PROFIT - CLASSIC
+        if takeProfitType == 1 and shortPosition and float(position_info["entryPrice"][len(position_info.index) - 1]) - (float(position_info["entryPrice"][len(position_info.index) - 1])/100) * takeProfit >= currentPrice and (positionSide == 2 or positionSide == 3):
             print("TAKE PROFIT")
             shortExit()
             first = True
+            shortTPtrigger = False
             tradeCount = 0
-            
-        # LONG STOP LOSS
-        if longPosition and (float(free_balance["USDT"]) <= baseOrderSize or maxSafetyTradesCount<=tradeCount) and firstPrice - (firstPrice/100) * stopLoss >= currentPrice and (positionSide == 1 or positionSide == 3):
-            print("STOP LOSS")
-            longExit()
-            first = True
-            tradeCount = 0
+            if mail == 1:
+                baslik = symbol
+                message = "SHORT TAKE PROFIT\n" + "TOTAL USDT: " + str(balance['total']["USDT"])
+                content = f"Subject: {baslik}\n\n{message}"
+                mail = SMTP("smtp.gmail.com", 587)
+                mail.ehlo()
+                mail.starttls()
+                mail.login(config.mailAddress, config.password)
+                mail.sendmail(config.mailAddress, config.mailAddress, content.encode("utf-8"))
+            takeProfitCount = takeProfitCount+ 1
+
+
+        # get lowest price
+        if currentPrice < lowestPrice:
+            lowestPrice = currentPrice
+        else: lowestPrice = lowestPrice
+
+        # SHORT TAKE PROFIT - TRAILING TRIGGER
+        if takeProfitType == 2 and shortPosition and float(position_info["entryPrice"][len(position_info.index) - 1]) - (float(position_info["entryPrice"][len(position_info.index) - 1])/100) * takeProfitTrigger >= currentPrice:
+            shortTPtrigger = True
         
-        # SHORT STOP LOSS
-        if shortPosition and (float(free_balance["USDT"]) <= baseOrderSize or maxSafetyTradesCount<=tradeCount) and ((firstPrice / 100) * stopLoss) + firstPrice <= currentPrice and (positionSide == 2 or positionSide == 3):
+        # SHORT TAKE PROFIT - TRAILING
+        if takeProfitType == 2 and shortTPtrigger and shortPosition and ((lowestPrice / 100) * stopLossTrailing) + lowestPrice <= currentPrice and (positionSide == 2 or positionSide == 3):
+            print("TRAILING STOP LOSS")
+            shortExit()
+            first = True
+            shortTPtrigger = False
+            tradeCount = 0
+            takeProfitCount = takeProfitCount + 1
+            if mail == 1:
+                baslik = symbol
+                message = "SHORT TRAILING STOP LOSS\n" + "TOTAL USDT: " + str(balance['total']["USDT"])
+                content = f"Subject: {baslik}\n\n{message}"
+                mail = SMTP("smtp.gmail.com", 587)
+                mail.ehlo()
+                mail.starttls()
+                mail.login(config.mailAddress, config.password)
+                mail.sendmail(config.mailAddress, config.mailAddress, content.encode("utf-8"))
+
+
+        # LONG STOP LOSS - CLASSIC
+        if SLselection == 1 and stopLossType == 1 and longPosition  and maxSafetyTradesCount<=tradeCount and lastOrderPrice - (lastOrderPrice/100) * stopLoss >= currentPrice and (positionSide == 1 or positionSide == 3):
+            print("STOP LOSS")
+            longExit()
+            longTPtrigger = False
+            first = True
+            tradeCount = 0
+            if mail == 1:
+                baslik = symbol
+                message = "LONG STOP LOSS\n" + "TOTAL USDT: " + str(balance['total']["USDT"])
+                content = f"Subject: {baslik}\n\n{message}"
+                mail = SMTP("smtp.gmail.com", 587)
+                mail.ehlo()
+                mail.starttls()
+                mail.login(config.mailAddress, config.password)
+                mail.sendmail(config.mailAddress, config.mailAddress, content.encode("utf-8"))
+
+        # LONG STOP LOSS - TRAILING
+        if SLselection == 1 and stopLossType == 2 and longPosition and maxSafetyTradesCount<=tradeCount and highestPrice - (highestPrice/100) * stopLossTrailing >= currentPrice and (positionSide == 1 or positionSide == 3):
+            print("TRAILING STOP LOSS")
+            longExit()
+            longTPtrigger = False
+            first = True
+            tradeCount = 0
+            if mail == 1:
+                baslik = symbol
+                message = "LONG TRAILING STOP LOSS\n" + "TOTAL USDT: " + str(balance['total']["USDT"])
+                content = f"Subject: {baslik}\n\n{message}"
+                mail = SMTP("smtp.gmail.com", 587)
+                mail.ehlo()
+                mail.starttls()
+                mail.login(config.mailAddress, config.password)
+                mail.sendmail(config.mailAddress, config.mailAddress, content.encode("utf-8"))
+        
+        # SHORT STOP LOSS - CLASSIC
+        if SLselection == 1 and stopLossType == 1 and shortPosition and maxSafetyTradesCount<=tradeCount and ((lastOrderPrice / 100) * stopLoss) + lastOrderPrice <= currentPrice and (positionSide == 2 or positionSide == 3):
             print("STOP LOSS")
             shortExit()
             first = True
+            shortTPtrigger = False
             tradeCount = 0
-            
-            
+            if mail == 1:
+                baslik = symbol
+                message = "SHORT STOP LOSS\n" + "TOTAL USDT: " + str(balance['total']["USDT"])
+                content = f"Subject: {baslik}\n\n{message}"
+                mail = SMTP("smtp.gmail.com", 587)
+                mail.ehlo()
+                mail.starttls()
+                mail.login(config.mailAddress, config.password)
+                mail.sendmail(config.mailAddress, config.mailAddress, content.encode("utf-8"))
+
+        # SHORT STOP LOSS- TRAILING
+        if SLselection == 1 and stopLossType == 1 and shortPosition and maxSafetyTradesCount<=tradeCount and ((lowestPrice / 100) * stopLoss) + lowestPrice <= currentPrice and (positionSide == 2 or positionSide == 3):
+            print("STOP LOSS")
+            shortExit()
+            first = True
+            shortTPtrigger = False
+            tradeCount = 0
+            if mail == 1:
+                baslik = symbol
+                message = "SHORT STOP LOSS\n" + "TOTAL USDT: " + str(balance['total']["USDT"])
+                content = f"Subject: {baslik}\n\n{message}"
+                mail = SMTP("smtp.gmail.com", 587)
+                mail.ehlo()
+                mail.starttls()
+                mail.login(config.mailAddress, config.password)
+                mail.sendmail(config.mailAddress, config.mailAddress, content.encode("utf-8"))
+
+
+        if takeProfitCount == profitAmount:
+            exit()
+
+        print("=================================== SETTINGS ===================================")
+        print("Pair: ", symbol, " Leverage: ", leverage)
+        print("Base Order Size: ", baseOrderSize, " Safety Order Size: ", safetyOrderSize)
+        print("Max Safety Trades Count: ", maxSafetyTradesCount, " Price Deviation: %"+str(priceDeviation))
+        print("Safety Order Step Scale: ", safetyOrderStepScale, " Safety Order Volume Scale: ", safetyOrderVolumeScale)
+        if takeProfitType == 1:
+            print("Take Profit Type Is Classic. Take Profit: %"+str(takeProfit))
+        if takeProfitType == 2:
+            print("Take Profit Type Is Trailing. Trigger: %"+str(takeProfitTrigger), " Take Profit: %"+str(takeProfitTrailing))
+        if SLselection == 2:
+            print("No Stop Loss")
+        if SLselection == 1:
+            if stopLossType == 1:
+                print("Stop Loss Type Is Classic. Stop Loss: %"+str(stopLoss))
+            if stopLossType == 2:
+                print("Stop Loss Type Is Trailing. Stop Loss: %"+str(stopLossTrailing))
+        if positionSide == 1:
+            print("Position Side Only Long")
+        if positionSide == 2:
+            print("Position Side Only Short")
+        if positionSide == 3:
+            print("Position Side Long and Short")
+        print("After triggering take profit",profitAmount - takeProfitCount,"times, the bot will stop working")
+        print("================================= INFORMATIONS =================================")
+        if longTPtrigger:
+            print("Long Take Profit Triggered")
+        if shortTPtrigger:
+            print("Short Take Profit Triggered")    
         if longPosition:
             print("In Long Position")
         if shortPosition:
             print("In Short Position")
         if inPosition:
-            print("Trade Count: ", tradeCount, " Avarege Price: ", float(position_info["entryPrice"][len(position_info.index) - 1]), " Free Usdt: ", round(float(free_balance["USDT"]),2), " Total Money: ", round(float(balance['total']["USDT"]),2))
+            print("Trade Count: ", tradeCount, " Avarege Price: ", float(position_info["entryPrice"][len(position_info.index) - 1]), " Free Usdt: ", round(float(free_balance["USDT"]),2), " Total USDT: ", round(float(balance['total']["USDT"]),2))
         if inPosition == False: 
-            print("Starting Price: ", firstPrice, " Current Price: ", currentPrice, " Total Money: ", round(float(balance['total']["USDT"]),2))
-        print("=======================================================================================================================================")
+            print("Starting Price: ", firstPrice, " Current Price: ", currentPrice, " Total USDT: ", round(float(balance['total']["USDT"]),2))
+        print("================================================================================")
 
     except ccxt.BaseError as Error:
         print ("[ERROR] ", Error )
